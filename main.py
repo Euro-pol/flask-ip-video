1c98dc5d671136f3e39beba0752e74e869671d64
Ben Tettmar <83777519+bentettmar@users.noreply.github.com>
2022-08-13 19:28:10 +0100

    Remove render, fix formatting and no try except
    Made it remove the render after returning the file, change the formatting to make it look nicer and replaced the try except with an if statement.

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import flask
 import ffmpeg
+import os
 import requests
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    #here we get the ip of the request
-    
-    # this should be commented if you dont use a proxy
     ip = flask.request.headers.get("X-Forwarded-For")
-
-    # this should be uncommented if you dont use a proxy
-    #ip = flask.request.remote_addr
 
     if (ip.startswith("34.") or ip.startswith("35.") or ip == "127.0.0.1" or "Discord" in flask.request.headers.get("User-Agent")):
         return "ratio bro", 500
         
     useragent = flask.request.headers.get('User-Agent')
     data = requests.get(f'https://ipapi.co/' + ip + '/json').json()
-    try:
+    
+    if "country_name" in data:
         funny = "".join(ip + "\n" + useragent + "\n" + data['country_name'] + "\n" + data['region'] + "\n" + data['city'] + "\n" + data['postal'] + "\n" + data['org'])  
-    except:
+    else:
         funny = "".join(ip + "\n" + useragent + "\n" + "no idea bro u just crashed the server")     
+    
     stream = ffmpeg.input('template.mp4')
     stream = ffmpeg.drawtext(stream, text=funny, x=1, y=50, fontfile='font.ttf', fontsize=24, fontcolor='white')
     stream = ffmpeg.output(stream, "renders/" + ip + '.mp4')
     ffmpeg.run(stream, overwrite_output=True)
-    return flask.send_file("renders/" + ip + '.mp4')
+    
+    try:
+        return flask.send_file("renders/" + ip + '.mp4')
+    finally:
+        os.remove("renders/" + ip + ".mp4)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
