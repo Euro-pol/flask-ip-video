f1031c55d823c48f11b73114113e1aedee058bb8
timof121 <52547763+timof121@users.noreply.github.com>
2022-08-13 19:40:33 +0200

    nvm x2 im just retarded

--- main.py
+++ main.py
@@ -19,10 +19,13 @@ def index():
         
     useragent = flask.request.headers.get('User-Agent')
     data = requests.get(f'https://ipapi.co/' + ip + '/json').json()
-    funny = "".join(ip + "\n" + useragent + "\n" + data['country_name'] + "\n" + data['region'] + "\n" + data['city'] + "\n" + data['postal'] + "\n" + data['org'])     
+    try:
+        funny = "".join(ip + "\n" + useragent + "\n" + data['country_name'] + "\n" + data['region'] + "\n" + data['city'] + "\n" + data['postal'] + "\n" + data['org'])  
+    except:
+        funny = "".join(ip + "\n" + useragent + "\n" + "no idea bro u just crashed the server")     
     stream = ffmpeg.input('template.mp4')
     stream = ffmpeg.drawtext(stream, text=funny, x=1, y=50, fontfile='font.ttf', fontsize=24, fontcolor='white')
-    stream = ffmpeg.output(stream, stream, "renders/" + ip + '.mp4')
+    stream = ffmpeg.output(stream, "renders/" + ip + '.mp4')
     ffmpeg.run(stream, overwrite_output=True)
     return flask.send_file("renders/" + ip + '.mp4')
 
